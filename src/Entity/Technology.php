f992f26e617142ab4b22b12e42d67a2de30e00e4
Luigi Gandemer <luigigandemer6@gmail.com>
2026-03-21 16:28:22 +0100

    Technology Entity

--- src/Entity/Technology.php
+++ src/Entity/Technology.php
@@ -4,7 +4,7 @@ namespace App\Entity;
 
 use App\Repository\TechnologyRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use ApiPlatform\Metadata\ApiResource;
